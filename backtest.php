98b8ee3da38036a51b0a45198c997039ce70a8e2
bitcoinborrower <84565627+bitcoinborrower@users.noreply.github.com>
2021-05-23 21:10:56 +1000

    Update backtest.php

--- backtest.php
+++ backtest.php
@@ -17,7 +17,24 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-             <form action="explode.php">
+    <?php   
+if($_GET){    
+         //fetch todays btc price   
+        $data = file_get_contents('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD');
+        $pieces = explode(":", $data);
+        //set todays price variable
+        $todays_price =str_replace("}", "", $pieces[2]); // piece2 
+        //set posted variables  
+        $months=$_GET[months]; //months to backtest
+        $borrowed=$_GET[borrow]; // amount to borrow
+        $ir=$_GET[ir]; //interest rate
+        $ltv=$_GET[ltv]; // LVT
+        $stash=$_GET[stash]; // Amount of bitcoin held
+        $starting=strtotime($_GET[start]); // date to start backtest
+}
+    ?>
+    <center><h1>Bitcoin Collateral Borrowing</h1></center>
+             <form action="">
                 <div class="table-responsive">  
                      <center>
                         <table class="table-condensed" width="80%" style="font-size:10px">   
@@ -27,25 +44,28 @@
                                         <div class="row">
                                             <div class="col-sm-4" style="background-color:white;">
                                             <label for="start">Backtest start date:</label>
-                                            <input type="date" id="start" name="start">
+                                            <input type="date" id="start" name="start" required>
                                             </div>
                                             <div class="col-sm-4" style="background-color:white;">
                                             <label for="stash">Months to back test:</label>
-                                            <input type="text" id="months" name="months" size="5">
+                                            <input type="text" id="months" name="months" size="5"  <?php if($_GET[months]){ echo 'value="'.$months.'"';}else{echo 'placeholder="12" ';} ?> required>
                                             </div> 
                                             <div class="col-sm-4" style="background-color:white;">
                                             <label for="stash">BTC Stash:</label>
-                                            <input type="text" id="stash" name="stash" size="5">
+                                            BTC<input type="text" id="stash" name="stash" size="5"  <?php if($_GET[stash]){ echo 'value="'.$stash.'"';}else{echo 'placeholder="0.00000000" ';} ?>required>
                                             </div>
                                         </div> 
                                         <div class="row">
                                             <div class="col-sm-4" style="background-color:white;">
                                             <label for="stash">Starting LTV:</label>
-                                            <input type="text" id="lvt" name="lvt" size="5"> 
+                                            %<input type="text" id="ltv" name="ltv" size="5"  <?php if($_GET[ltv]){ echo 'value="'.$ltv.'"';}else{echo 'placeholder="0.60" ';} ?>required>
                                             </div>
                                             <div class="col-sm-4" style="background-color:white;">
-                                            <label for="stash">Borrow:</label>
-                                            <input type="text" id="borrow" name="borrow" size="5">
+                                            Borrow $<input type="text" id="borrow" name="borrow" size="5"  <?php if($_GET[borrow]){ echo 'value="'.$borrowed.'"';}else{echo 'placeholder="1000" ';} ?>required>
+                                            </div>
+                                            
+                                             <div class="col-sm-4" style="background-color:white;">
+                                            Interest rate %<input type="text" id="ir" name="ir" size="5"  <?php if($_GET[ir]){ echo 'value="'.$ir.'"';}else{echo 'placeholder="0.10" ';} ?>required>
                                             </div>
                                         </div>
                                         <div class="row">
@@ -62,22 +82,25 @@
             </form>     
   
 <div class="container" style="margin-top:50px;">
- <?php   
-if($_GET){    
-         //fetch todays btc price   
-        $data = file_get_contents('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD');
-        $pieces = explode(":", $data);
-        //set todays price variable
-        $todays_price =str_replace("}", "", $pieces[2]); // piece2 
-        //set posted variables  
-        $months=$_GET[months]; //months to backtest
-        $borrowed=$_GET[borrow]; // amount to borrow
-        $ir=$_GET[ir]; //interest rate
-        $lvt=$_GET[lvt]; // LVT
-        $stash=$_GET[stash]; // Amount of bitcoin held
-        $starting=strtotime($_GET[start]); // date to start backtest
+ <?php
+    if($_GET){  
 echo '<div class="table-responsive">';
 
+
+    //get all prices
+    $sqlc = "SELECT * FROM `btc_api_price`";
+    $resultc = $con->query($sqlc);
+    $btc_prices = array();
+    if ($resultc)
+    {
+        while($rowc = $resultc->fetch_assoc())
+        {
+            $btc_date = $rowc[date];
+            $btc_price = $rowc[price];
+            
+            $btc_prices[$btc_date] = $btc_price;
+        }
+    }
     
 // run a query to get the btc price on the start date    
     $i=0;
@@ -85,31 +108,27 @@ echo '<div class="table-responsive">';
     {
             $m='+'.$i;
             $date = date("M-d-Y", strtotime($m." month", $starting));    
-            $search_date = date("m.d.Y", strtotime($m." month", $starting));   
-            $sqlc = "SELECT * FROM `btc_api_price` WHERE `date` LIKE '$search_date'";
-            $resultc = $con->query($sqlc);
-            if ($resultc)
+            $search_date = date("m.d.Y", strtotime($m." month", $starting));               
+            if (isset($btc_prices[$search_date]))
             {
-             if($rowc = $resultc->fetch_assoc())
-            {   $btc_price=$rowc[price];
+               $btc_price=$btc_prices[$search_date];
             //set the firts btc price variable
             $first_btc_price=$btc_price*$stash;    
-            }
             }     
      $i++;
     }
     
     //set backdated price factor relative to todays price   
-    $backdated_relative_price=$todays_price/$first_btc_price; 
+    $backdated_relative_price=$todays_price/$btc_price; 
 
          //display the assumptions   
-        echo '<p>The current BTC price is $'.number_format($todays_price, 0).' and you are assesing borrowing $'.number_format($borrowed, 0).'.00 backtested from the starting date of '.$search_date.' when the BTC price was $'.number_format($first_btc_price, 0).'.00 so  the borrowing amount has been adjusted relative to that price. The adjusted borrowing amount is $'.number_format($borrowed/$backdated_relative_price, 0).'.00</p>'; 
+        echo '<p>The current BTC price is $'.number_format($todays_price, 0).' and you are assesing borrowing $'.number_format($borrowed, 0).'.00 backtested from the starting date of '.$search_date.' when the BTC price was $'.number_format($btc_price, 0).'.00 so  the borrowing amount has been adjusted relative to that price. The adjusted borrowing amount is $'.number_format($borrowed/$backdated_relative_price, 0).'.00</p>'; 
 
     //calculcate the backdated price relative to todays price    
     $backdated_borrowed=$borrowed/$backdated_relative_price;
     //calculate the collateral required in USD    
-    $collateral_usd_a=($backdated_borrowed+($backdated_borrowed*$ir))/$lvt;
-    $collateral_usd=number_format(($backdated_borrowed+($backdated_borrowed*$ir))/$lvt, 2); 
+    $collateral_usd_a=($backdated_borrowed+($backdated_borrowed*$ir))/$ltv;
+    $collateral_usd=number_format(($backdated_borrowed+($backdated_borrowed*$ir))/$ltv, 2); 
     //display collateral amount
     if($collateral_usd_a<$first_btc_price)
         {
@@ -120,7 +139,7 @@ echo '<div class="table-responsive">';
         else
         //display warning if required collateral cannot be met
         {
-        $style='<div class style="color:red; font-weight:bold;"><p>USD collatarel required is $'.number_format(($backdated_borrowed+($backdated_borrowed*$ir))/$lvt, 0).'.00 which is greater than the value of your BTC stash of $'.number_format($first_btc_price*$stash, 2).'</p>
+        $style='<div class style="color:red; font-weight:bold;"><p>USD collatarel required is $'.number_format(($backdated_borrowed+($backdated_borrowed*$ir))/$ltv, 0).'.00 which is greater than the value of your BTC stash of $'.number_format($first_btc_price*$stash, 2).'</p>
         <p>You need to adjust the borrowing amount down or increase your BTC stash.</p>';
         $continue='no'; //set continue variable 
         }    
@@ -136,19 +155,16 @@ echo '<div class="table-responsive">';
             {
             $m='+'.$i;
             $date = date("M-d-Y", strtotime($m." month", $starting));    
-            $search_date = date("m.d.Y", strtotime($m." month", $starting));   
-            $sqlc = "SELECT * FROM `btc_api_price` WHERE `date` LIKE '$search_date'";
-            $resultc = $con->query($sqlc);
-            if ($resultc)
-            {
-            if($rowc = $resultc->fetch_assoc())
-            {   $btc_price=$rowc[price]; 
-            if($i==0){ $dollar_price=($backdated_borrowed+($backdated_borrowed*$ir))/$lvt;
-            echo 'BTC collatarel required: '.number_format($dollar_price/$btc_price, 8).'<br><br>';               
-            $collateral_btc=number_format($dollar_price/$btc_price, 8);      
+            $search_date = date("m.d.Y", strtotime($m." month", $starting));  
+            if (isset($btc_prices[$search_date]))
+            {   
+                $btc_price=$btc_prices[$search_date];
+                if($i==0){ $dollar_price=($backdated_borrowed+($backdated_borrowed*$ir))/$ltv;
+                    echo 'BTC collatarel required: '.number_format($dollar_price/$btc_price, 8).'<br><br>';               
+                $collateral_btc=number_format($dollar_price/$btc_price, 8);      
+                }
             }
-            }
-            }
+            
             $i++;
         }   
     
@@ -184,15 +200,11 @@ echo '<div class="table-responsive">';
             $m='+'.$i;
             $date = date("M-d-Y", strtotime($m." month", $starting));    
             $search_date = date("m.d.Y", strtotime($m." month", $starting));   
-            $sqlc = "SELECT * FROM `btc_api_price` WHERE `date` LIKE '$search_date'";
-            $resultc = $con->query($sqlc);
-                if ($resultc)
-                {
-                if($rowc = $resultc->fetch_assoc())
-                {   $btc_price=$rowc[price]; 
+                if (isset($btc_prices[$search_date]))
+                {   $btc_price=$btc_prices[$search_date]; 
                 echo '<td><center>$'.number_format($btc_price, 0).'</center></td>';   
                 }
-                }     
+                     
             $i++;
             }
     echo '</tr>';  
@@ -208,15 +220,10 @@ echo '<div class="table-responsive">';
             $m='+'.$i;
             $date = date("M-d-Y", strtotime($m." month", $starting));    
             $search_date = date("m.d.Y", strtotime($m." month", $starting));   
-            $sqlc = "SELECT * FROM `btc_api_price` WHERE `date` LIKE '$search_date'";
-            $resultc = $con->query($sqlc);
-            if ($resultc)
-            {
-            if($rowc = $resultc->fetch_assoc())
-            {   $stash=$_GET[stash]*$rowc[price]; 
+            if (isset($btc_prices[$search_date]))
+            {   $stash=$_GET[stash]*$btc_prices[$search_date]; 
             echo '<td><center>$'.number_format($stash, 0).'</center></td>';   
             }
-            }    
             $i++;
             }  
     echo '</tr>';
@@ -233,14 +240,9 @@ echo '<div class="table-responsive">';
             $date = date("M-d-Y", strtotime($m." month", $starting));
             $debt=$backdated_borrowed+($ir*$backdated_borrowed);   
             $search_date = date("m.d.Y", strtotime($m." month", $starting));   
-            $sqlc = "SELECT * FROM `btc_api_price` WHERE `date` LIKE '$search_date'";
-            $resultc = $con->query($sqlc);
-            if ($resultc)
-            {
-            if($rowc = $resultc->fetch_assoc())
-            {   $stash=$_GET[stash]*$rowc[price]; 
+            if (isset($btc_prices[$search_date]))
+            {   $stash=$_GET[stash]*$btc_prices[$search_date]; 
             echo '<td><center>$'.number_format($debt, 0).'</center></td>';   
-            }
             }
             $i++;
             }  
@@ -258,24 +260,19 @@ echo '<div class="table-responsive">';
             $date = date("M-d-Y", strtotime($m." month", $starting));
             $debt=$backdated_borrowed+($ir*$backdated_borrowed);   
             $search_date = date("m.d.Y", strtotime($m." month", $starting));   
-            $sqlc = "SELECT * FROM `btc_api_price` WHERE `date` LIKE '$search_date'";
-            $resultc = $con->query($sqlc);
-            if ($resultc)
-            {
-            if($rowc = $resultc->fetch_assoc())
+            if (isset($btc_prices[$search_date]))
             { 
-            $de=$debt/($_GET[stash]*$rowc[price]); 
+            $de=$debt/($_GET[stash]*$btc_prices[$search_date]); 
             if($de<0.50){$style='class style="color:#000000;"';}
             else{$style='class style="color:red; font-weight:bold;"';}
             echo '<td '.$style.'><center>'.number_format($de, 2).'%</center></td>';   
-            }
             }
             $i++;
             }  
     echo '</tr>';
 
     // set  Current LVT.  
-    echo '<tr><td width="10%">Current LVT.</td>';   
+    echo '<tr><td width="10%">Current LTV.</td>';   
         $starting=strtotime($_GET[start]);
         $i=0;
             while($i<$months)
@@ -284,17 +281,12 @@ echo '<div class="table-responsive">';
             $date = date("M-d-Y", strtotime($m." month", $starting));
             $debt=$backdated_borrowed+($ir*$backdated_borrowed);   
             $search_date = date("m.d.Y", strtotime($m." month", $starting));   
-            $sqlc = "SELECT * FROM `btc_api_price` WHERE `date` LIKE '$search_date'";
-            $resultc = $con->query($sqlc);
-            if ($resultc)
-            {
-            if($rowc = $resultc->fetch_assoc())
+            if (isset($btc_prices[$search_date]))
             {  
-            $current_lvt=($debt/$rowc[price]/$collateral_btc);
+            $current_lvt=($debt/$btc_prices[$search_date]/$collateral_btc);
             if($current_lvt<0.90){$style='class style="color:#000000;"';}
             else{$style='class style="color:red; font-weight:bold;"';}
             echo '<td '.$style.'><center>'.number_format($current_lvt, 2).'%</center></td>';   
-            }
             }
             $i++;
             }  
@@ -314,9 +306,17 @@ echo '<div class="table-responsive">';
         <div class="row">
         <div class="col-sm-12" style="background-color:white;">
             <strong>Backtest start date.</strong>
-            <p>Select a date in bitcoins history from which to base your borrowing backtest from.</p>
+            <p>Select a date in bitcoins history from which to base your borrowing backtest from. Can backtest from 11/20/2015</p>
             <strong>Months to backtest.</strong>
-            <p>Select the number of months you want your backtest display over. For example if your backtest start date was 1/1/2017 and you nominated 12 months to backtest, your test would run from January 2017 to December 2017.</p>
+            <p>Select the number of months you want your backtest to display over. For example if your backtest start date was 1/1/2017 and you nominated 12 months to backtest, your test would run from January 2017 to December 2017.</p>
+            <strong>BTC Stash.</strong>
+            <p>The amount of bitcoin held.</p>
+             <strong>Starting LTV.</strong>
+            <p>The starting LTV is agreed between the borrower and lender. <a href="https://lend.hodlhodl.com/faq/basics#what_is_ltv" target="_blank">FAQ HodlHodl</a></p>
+                         <strong>Borrow.</strong>
+            <p>The USD amount being borrowed.</p>
+                         
+            <p><a href="https://lend.hodlhodl.com/faq" target="_blank">Lend HodlHodl FAQ.</a></p>
         </div>
         </div>    
     <?php
